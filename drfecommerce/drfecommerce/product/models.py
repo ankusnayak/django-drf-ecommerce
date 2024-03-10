56dc448026668fbfc80777aa131331d32ade5733
Ankus Nayak <iamankus7@gmail.com>
2024-03-11 02:59:29 +0530

    Define Models and Serializers

--- drfecommerce/drfecommerce/product/models.py
+++ drfecommerce/drfecommerce/product/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+# import uuid
 
-# Create your models here.
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Category(MPTTModel):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    is_digital = models.BooleanField(default=False)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = TreeForeignKey(
+        "Category", on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.name
